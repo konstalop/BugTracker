bc84c51345cc3e8ddcfc4cd1310d7fef21c6d01e
Konsta <konlopponen@gmail.com>
2022-07-19 14:49:27 +0300

    add jwt to authorization, create middleware to verify jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const router = require('./users')
+const jwt = require('jsonwebtoken')
 
 //Handle login
 router.post('/login', async (req, res) => {
@@ -12,8 +13,16 @@ router.post('/login', async (req, res) => {
 
     const validPw = await bcrypt.compare(req.body.password, user.password)
 
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn: 3600})
+
     if (validPw) {
-        return res.send('Succesfully logged in!')
+        return res.json({token: accessToken})
     } else {
         return res.status(401).json('Invalid login!')
     }
