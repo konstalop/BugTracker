4a91113b9e772f64827d5955a812da57d1c2da04
Konsta <konlopponen@gmail.com>
2022-07-29 00:03:32 +0300

    deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.DB_URL).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log('connected to mongodb')
 })
 .catch((err) => {
@@ -26,5 +27,12 @@ app.use('/tickets', ticketsRouter)
 app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => console.log(`Server alive on port ${PORT}`))
