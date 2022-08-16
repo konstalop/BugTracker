b60624634e15fcd52962f6a45a756d423deba3b9
Konsta <konlopponen@gmail.com>
2022-08-16 17:25:59 +0300

    finish e2e test for landing

--- client/cypress/e2e/landing.cy.js
+++ client/cypress/e2e/landing.cy.js
@@ -18,4 +18,16 @@ describe('Landing page', () => {
         cy.get('.form-auth').should('exist').contains('Sign up')
         cy.get('.new-user-text').click()
     })
+
+    it('Test a failed login attempt alert', () => {
+        cy.get('.input-auth-submit').click()
+        cy.get('.alert-container').should('exist').contains('Please fill all of the fields')
+    })
+
+    it('Test wrong credentials', () => {
+        cy.get('.input-auth').eq(0).type('test@test.com')
+        cy.get('.input-auth').eq(1).type('123')
+        cy.get('.input-auth-submit').click()
+        cy.get('.alert-container').should('exist').contains('Invalid login!')
+    })
 }) 
